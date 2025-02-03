81a9611e6470fd3ed6d5e7133aef8180d551cd3c
Shubham Tanwar <mr.tanwar2020@gmail.com>
2025-02-03 18:11:25 +0530

    removed unused code

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,23 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Facebook</title>
+        <meta name="Facebook" content="This is clone of Faecbook." />
+
+        <link rel="icon" href="/favicon.png" />
+        {/* Fonts added */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Helvetica+Neue:wght@300;400;500;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
