a4bfd625c02650eb2cd7b637117bd580bc8e4d1c
mr-tanwar <mr.tanwar2020@gmail.com>
2025-07-20 21:59:31 +0530

    feat: css added for errors on  Login page

--- src/features/auth/component/loginForm/LoginForm.tsx
+++ src/features/auth/component/loginForm/LoginForm.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 
-import clsx from 'clsx';
 import { useTranslation } from 'next-i18next';
 import { SubmitHandler, useForm } from 'react-hook-form';
 
-import { Divider, InputField } from '@/components/ui';
+import { Divider } from '@/components/ui';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
 
 import { LoginFormProps } from './types';
 
 import { REGEX } from '@/utils';
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
 
 interface FormProps {
   email: string;
@@ -39,68 +38,74 @@ const LoginForm: React.FC<LoginFormProps> = ({ signUpHandler }) => {
   };
 
   return (
-    <div className={''}>
-      <form onSubmit={handleSubmit(onSubmitHandler)} noValidate>
-        <div>
-          <Input
-            type="email"
-            placeholder={t('emailPhoneNumber')}
-            {...register('email', {
-              required: t('emailRequiredErr'),
-              pattern: {
-                value: REGEX.EMAIL,
-                message: t('invalidEmailErr'),
-              },
-            })}
-            className="mb-4 pb-8 pt-8 md:text-lg"
-          />
-          {errors.email && <p className={''}>{errors.email.message}</p>}
-        </div>
-        <div>
-          <Input
-            type="password"
-            placeholder={t('password')}
-            {...register('password', {
-              required: t('passwordRequiredErr'),
-              minLength: {
-                value: 6,
-                message: t('passwordMinLengthErr'),
-              },
-              pattern: {
-                value: REGEX.PASSWORD,
-                message: t('passwordErr'),
-              },
-            })}
-            className="mb-4 pb-8 pt-8 font-medium md:text-lg"
-          />
-          {errors.password && <p className={''}>{errors.password.message}</p>}
-        </div>
+    <form onSubmit={handleSubmit(onSubmitHandler)} noValidate>
+      <div>
+        <Input
+          type="email"
+          placeholder={t('emailPhoneNumber')}
+          {...register('email', {
+            required: t('emailRequiredErr'),
+            pattern: {
+              value: REGEX.EMAIL,
+              message: t('invalidEmailErr'),
+            },
+          })}
+          className="mb-4 pb-8 pt-8 md:text-lg"
+        />
+        {errors.email && (
+          <p className={'mb-2 font-medium text-red-600'}>
+            {errors.email.message}
+          </p>
+        )}
+      </div>
+      <div>
+        <Input
+          type="password"
+          placeholder={t('password')}
+          {...register('password', {
+            required: t('passwordRequiredErr'),
+            minLength: {
+              value: 6,
+              message: t('passwordMinLengthErr'),
+            },
+            pattern: {
+              value: REGEX.PASSWORD,
+              message: t('passwordErr'),
+            },
+          })}
+          className="mb-4 pb-8 pt-8 font-medium md:text-lg"
+        />
+        {errors.password && (
+          <p className="mb-2 font-medium text-red-600">
+            {errors.password.message}
+          </p>
+        )}
+      </div>
 
+      <Button
+        onClick={() => {}}
+        className="mb-4 w-full p-6 text-lg font-medium"
+      >
+        {' '}
+        {t('login')}{' '}
+      </Button>
+      <Button
+        onClick={() => {}}
+        variant={'link'}
+        className="w-full justify-center text-center font-medium"
+      >
+        {t('forgotPassword')}
+      </Button>
+      <Divider />
+      <div className="w-full text-center">
         <Button
-          onClick={() => {}}
-          className="mb-4 w-full p-6 text-lg font-medium"
+          onClick={signUpHandler}
+          className="mt-8 bg-green-600 p-6 text-lg font-medium"
         >
-          {' '}
-          {t('login')}{' '}
+          {t('createNewAccount')}
         </Button>
-        <Button
-          onClick={() => {}}
-          variant={'link'}
-          className="w-full justify-center text-center font-medium"
-        >
-          {t('forgotPassword')}
-        </Button>
-        <Divider />
-        <div className="w-full text-center">
-          <Button
-            onClick={signUpHandler}
-            className="mt-8 bg-green-600 p-6 text-lg font-medium"
-          >
-            {t('createNewAccount')}
-          </Button>
-        </div>
-      </form>
-    </div>
+      </div>
+    </form>
   );
 };
 
